eb93bdd2f129639aa53efe4015474d3cf34b9e88
Masum Sakib <masumsakib396@gmail.com>
2020-06-27 05:56:00 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+(function($) {
+"use strict";
+
+/*------------------------------------------------------------------
+[Table of contents]
+
+
+-------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------
+CUSTOM PRE DEFINE FUNCTION
+------------------------------------------------------------*/
+/* is_exist() */
+jQuery.fn.is_exist = function(){
+  return this.length;
+}
+
+
+$(function(){
+
+  
+
+});/*End document ready*/
+
+
+$(window).on("resize", function(){
+
+}); // end window resize
+
+
+$(window).on("load" ,function(){
+
+
+
+}); // End window LODE
+
+
+
+
+})(jQuery);
+
+
+
+
+
+
